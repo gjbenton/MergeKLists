94a55eed0d43e92d69cf0bda79b1dd793ae1c7fc
gjbenton <61299519+gjbenton@users.noreply.github.com>
2021-01-14 14:56:46 -1000

    Add files via upload

--- KLists.java
+++ KLists.java
@@ -1,72 +1,49 @@
 import java.util.List;
 public class KLists{
-	//double[][] klists= {{1.1,4.4,5.5},{1.1,3.3,4.4},{2.2,6.6,8.8}}; //8.8 doesn't belong but wanted to make it work with eqaul size strings
 	public double[] mergeKLists (double[][] outerArray){
-		//count will be used to get the size of the sorted array & increment target index
-		//make a helper function (later)
-		int count = 0;
-		for(int i=0; i<outerArray.length; i++){
-			for(int j=0; j<outerArray[i].length; j++){
-				count += 1;
-			}
+		double[] sorted = {};
+		int i = 0;
+		while(i < outerArray.length){
+			double tempArr [] = new double[sorted.length + outerArray[i].length];
+			merge(sorted, outerArray[i], tempArr);
+			sorted = tempArr;
+			i++;
 		}
-		//System.out.println("Count = " +count);
-		
-		double[] sorted = new double[count];
-		int[] indexO = new int[outerArray.length];
-		int target_index = 0;
-
-		
-		//while(sorted.length > target_index){
-			// for(int i=0; i<indexO.length-1; i++){	
-			// 	sorted[target_index] = getSortedIndex(outerArray, indexO);
-			// 	target_index++;
-			// }
-		//}
-		for(int i=0; i<indexO.length-1; i++){
-			while(indexO[i] < outerArray[i].length && indexO[i+1] < outerArray[i+1].length)	
-				//if(outerArray[i].length)
-				sorted[target_index] = getSortedIndex(outerArray, indexO, i);
-				target_index++;
-			}
-		
-			// while(outerArray[i].length > indexO[i] && outerArray[i+1].length > indexO[i]){
-			// if(outerArray[i][i]< outerArray[i+1][i]){
-			// 	//smallest = outerArray[i][i];
-			// 	sorted[target_index] = outerArray[i][i];
-			// }
-			// indexO[i] +=1;
-			// target_index++;
-
-			// }
 		return sorted;
 	}
-	public double getSortedIndex(double[][] outerArray, int[] indexO, int k){
-		//int k=0;
-		double smallest= outerArray[0][indexO[0]];
-		for(int i=0; i<indexO.length; i++){
-			if(indexO[i] < outerArray[i].length && outerArray[i][indexO[i]]< smallest)
-				smallest = outerArray[i][indexO[i]];	
+	public void merge(double[] left, double[] right, double[] target){
+		int left_index = 0;
+		int right_index = 0;
+		int target_index = 0;
+
+		//compares left array with right array (both sorted)
+		while(left_index < left.length && right_index < right.length){
+			if(left[left_index] > right[right_index]){
+				target[target_index] = right[right_index];
+				target_index++;
+				right_index++;
+			}
+			else{
+				target[target_index] = left[left_index];
+				target_index++;
+				left_index++;
+			}
 		}
-		//k++;
-		return smallest;
+		while(left_index< left.length){
+			target[target_index] = left[left_index];
+		target_index++;
+		left_index++;
+		}
+		while(right_index < right.length){
+			target[target_index] = right[right_index];
+		target_index++;
+		right_index++;
+		}
 	}
-	// public String toString(){
-	// 	String output =" ";
-	// 	for(int i=0; i<klists.length; i++){
-	// 		output += "[";
-	// 		for(int j=0; j<klists.length; j++){
-	// 			output += klists[i][j];
-	// 			if(j!= klists[i].length-1)
-	// 				output += ", ";
-	// 		}
-	// 		output += "]";
-	// 	}
-	// 	return output;
-	// }
+
 	public static void main(String[] args){
 		KLists klist = new KLists();
-		double[][] list = {{1.1,4.4,5.5},{1.1,3.3,4.4},{2.2,6.6,8.8}};
+		double[][] list = {{1.1,4.4,5.5},{1.1,3.3,4.4},{2.2,6.6},{1.1, 2.2}};
 		double[] merged = klist.mergeKLists(list);
 
 		for(int i=0; i<merged.length; i++){
